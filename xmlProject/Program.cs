b0907b5a26e72963bc63fd493332bad65bac529e
Anton Iudin <yudin431@gmail.com>
2022-01-02 20:43:56 -0500

    General Update

---
+++ xmlProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+
+namespace xmlProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
